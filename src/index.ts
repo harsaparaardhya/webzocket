4ee3e004305b266bf1eae06f91a1e083cccb8d6c
Harsa Paraardhya <harsa.paraardhya@gmail.com>
2020-08-03 20:06:20 +0700

    0.0.12

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
+let socket: any = false;
 const receivers: any[] = [];
 
+const WebZocket = {
+  init: (conf: string) => {
+    const configs = window.atob(conf);
+    const config = configs.split("::");
+    socket = new WebSocket(config[0], config[1]);
+    socket.onopen = (e: any) => {};
+
+    socket.onmessage = (event: any) => {
+      const data = JSON.parse(event.data);
+
+      for (const a in receivers) {
+        const receiver = receivers[a];
+        if (data.key == receiver.key) receiver.callback(data.value);
+      }
+    };
+
+    socket.onclose = () => {
+      socket = null;
+      setTimeout(WebZocket.init, 5000);
+    };
+
+    socket.onerror = (error: any) => {
+      console.log(error);
+    };
+  },
+  trigger: (key: string, destination: string, value: any) => {
+    const message = {
+      key: key,
+      destination: destination,
+      value: value,
+    };
+
+    if (socket) socket.send(JSON.stringify(message));
+  },
+  receiver: (key: string, callback: (a: any) => void) => {
+    const receiver = {
+      key: key,
+      callback: callback
+    };
+    receivers.push(receiver);
+  }
+};
+
+/*
 class WebZocket {
   private socket: any = false;
 
@@ -48,5 +93,6 @@ class WebZocket {
     receivers.push(receiver);
   }
 }
+*/
 
 export { WebZocket };
