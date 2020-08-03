349ab672e77c0807de8a298f699077997ab82ae2
Harsa Paraardhya <harsa.paraardhya@gmail.com>
2020-08-03 10:09:12 +0700

    0.0.3

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,10 @@
-export declare function add(a: number, b: number): number;
+declare class WebZocket {
+  socket: any;
+  receivers: any[];
+  constructor();
+  init(room: string): void;
+  trigger(key: string, destination: string, value: any): void;
+  receiver(key: string, callback: any): void;
+}
+export { WebZocket };
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,70 @@
-export function add(a: number, b: number) {
-  return a + b;
+class Message {
+  msg: string;
+  constructor(
+    public key: string,
+    public destination: string,
+    public value: any
+  ) {
+    const message = {
+      key: key,
+      destination: destination,
+      value: value,
+    };
+    this.msg = JSON.stringify(message);
+  }
 }
+
+class Receiver {
+  rcv: any;
+  constructor(public key: string, public callback: any) {
+    const receiver = {
+      key: key,
+      callback: callback,
+    };
+    this.rcv = receiver;
+  }
+}
+
+class WebZocket {
+  socket: any = false;
+  receivers: any[] = [];
+
+  constructor() {}
+
+  init(room: string) {
+    this.socket = new WebSocket("ws://localhost:8080/", room);
+
+    this.socket.onopen = (e: any) => {};
+
+    this.socket.onmessage = (event: any) => {
+      const data = JSON.parse(event.data);
+
+      for (let a = 0; a < this.receivers.length; a++) {
+        const receiver = this.receivers[a];
+        if (data.key == receiver.key) receiver.callback(data.value);
+      }
+    };
+
+    this.socket.onclose = () => {
+      this.socket = null;
+      setTimeout(this.init, 5000);
+    };
+
+    this.socket.onerror = (error: any) => {
+      console.log(error);
+    };
+  }
+
+  trigger(key: string, destination: string, value: any) {
+    const message = new Message(key, destination, value);
+
+    if (this.socket) this.socket.send(message.msg);
+  }
+
+  receiver(key: string, callback: any) {
+    const receiver = new Receiver(key, callback);
+    this.receivers.push(receiver.rcv);
+  }
+}
+
+export { WebZocket };
